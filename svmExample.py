9281f27622a6df41c71de95ecceea99f0776d1e6
TrentBrunson <66187531+TrentBrunson@users.noreply.github.com>
2020-09-25 22:17:25 -0500

    Add files via upload

---
+++ svmExample.py
@@ -0,0 +1,36 @@
+#%%
+from sklearn.datasets.samples_generator import make_blobs
+import matplotlib.pyplot as plt
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.svm import SVC
+from sklearn.metrics import classification_report
+# %%
+attrVector, labelVector = make_blobs(centers = 2, n_samples = 500, cluster_std=1.95)
+# %%
+plt.scatter(attrVector[:,0], attrVector[:,1], c=labelVector)
+# %%
+X_train, X_test, y_train, y_test = train_test_split(attrVector,labelVector)
+# %%
+svcInstance: SVC = SVC(kernel='rbf')
+# %%
+paramsToTestDict: dict = {
+    "C" : [1.0,5.0,10.0,50.0,100.0],
+    "gamma" : [0.0001,0.0005,0.1,0.5,1.0]
+    }
+
+paramSearchGrid: GridSearchCV = GridSearchCV(
+    estimator = svcInstance, 
+    param_grid=paramsToTestDict
+    )
+
+
+# svcInstance.fit(X_train, y_train)
+# %%
+paramSearchGrid.fit(X_train,y_train)
+#%%
+print(paramSearchGrid.best_params_)
+#%%
+predictedLabels = paramSearchGrid.predict(X_test)
+#%%
+print(classification_report)
